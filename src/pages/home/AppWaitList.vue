4ee1769a8eb4688067d38247bdd9ebe09642e735
artush-alexanyan <artush.alexanyan1989@gmail.com>
2024-05-24 15:30:11 +0400

    fix:waitlist

--- src/pages/home/AppWaitList.vue
+++ src/pages/home/AppWaitList.vue
@@ -4,28 +4,30 @@
       <div class="image_wrapper md:w-1/2 w-full">
         <img src="@/assets/images/section-4/cup.png" alt="cup" />
       </div>
-      <div class="md:w-1/2 w-full">
+      <div class="md:w-3/5 w-full">
         <div class="wishlist_wrapper md:w-5/6 w-full">
           <h2
-            class="lg:text-[64px] md:text-4xl text-2xl md:text-left text-center text-white uppercase font-vintage leading-none"
+            class="lg:text-[64px] md:text-4xl text-2xl md:text-left text-center text-white uppercase font-vintage"
           >
             Be <span class="text-[#D3041C]">First</span>
           </h2>
-          <p class="my-3 md:text-sm text-xs md:text-left text-center text-[#ABABABE5]">
+          <p class="md:text-sm text-xs md:text-left text-center text-[#ABABABE5] mt-10 mb-7">
             Vitae orci odio id at. Euismod vitae tellus bibendum amet iaculis libero aliquam.
             Consectetur ridiculus tristique non vestibulum aenean arcu facilisis. Viverra sit ut
             venenatis integer sem velit cras varius viverra. Nibh quisque lacus velit pellentesque.
             Fames habitant scelerisque euismod non amet tempor. Semper sit quisque quisque tristique
             habitant arcu. Velit id commodo proin semper quam vel nulla lorem. Eros volutpat.
           </p>
-          <div class="md:flex md:items-center md:w-full md:space-x-5 md:spac-y-0 space-y-2.5">
+          <div
+            class="md:flex md:items-center justify-between xl:w-5/6 w-full md:space-x-5 md:space-y-0 space-y-2.5 lg:px-10"
+          >
             <input
               placeholder="Your Email"
               type="text"
-              class="rounded-full border border-[#ABABAB] md:w-auto w-full py-3 px-6 bg-transparent text-white placeholder:text-white"
+              class="rounded-full border border-[#ABABAB] w-full py-3 px-6 bg-transparent text-white placeholder:text-white"
             />
-            <button class="px-6 py-3 bg-[#D3041C] text-white md:w-auto w-full rounded-full">
-              Join Wishlist
+            <button class="px-6 py-3 md:w-1/2 w-full bg-[#D3041C] text-white rounded-full">
+              Join the Wishlist
             </button>
           </div>
         </div>
